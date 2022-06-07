ef6c7d89d0a26fc0e185c8c84d0b1f45061784da
Saied Gaffer <saied2421998@gmail.com>
2022-06-07 17:07:21 +0200

    create controller-view-route for specific user to show

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-   get 'users/index'
+   get 'users', to: 'users#index'
+   get 'users/:id' , to: 'users#show' , as: 'user'
   # Defines the root path route ("/")
-  # root "articles#index"
   root 'users#index'
 end
