24c7a9a041bf3c22138ede98fd0b1836b852f14a
Saied Gaffer <saied2421998@gmail.com>
2022-06-07 18:22:13 +0200

    add optional change

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-   get 'users', to: 'users#index'
-   get 'users/:id' , to: 'users#show' , as: 'user'
-   get 'users/:user_id/posts', to: 'posts#index', as: 'user_posts'
-   get 'users/:user_id/posts/:id', to: 'posts#show', as: 'user_post'
+   resources :users, only: [:index, :show] do
+   resources :posts, only: [:index, :show]
+  end
   # Defines the root path route ("/")
   root 'users#index'
 end
