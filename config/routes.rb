af0c6da71c078083b91a822180a711c2fe791a2e
Saied Gaffer <saied2421998@gmail.com>
2022-06-07 17:15:17 +0200

    create controller-view-route for posts of user

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
    get 'users', to: 'users#index'
    get 'users/:id' , to: 'users#show' , as: 'user'
+   get 'users/:user_id/posts', to: 'posts#index', as: 'user_posts'
   # Defines the root path route ("/")
   root 'users#index'
 end
