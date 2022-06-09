9d28519966a560af26b0a5eeca110e617d68f75f
Saied Gaffer <saied2421998@gmail.com>
2022-06-09 14:04:56 +0200

    create post table dpending on migration

--- db/migrate/20220609104031_create_post.rb
+++ db/migrate/20220609104031_create_post.rb
@@ -1,14 +1,12 @@
 class CreatePost < ActiveRecord::Migration[7.0]
   def change
     create_table :post do |t|
-      t.bigint :AuthorId
+      t.references :Author, index: true, foreign_key: {to_table: :user}
       t.string :Title, :limit => 32
       t.text :Text
       t.timestamps
       t.bigint :CommentsCounter
       t.bigint :LikesCounter
-      add_foreign_key :post, :user, column: :AuthorId, primary_key: "id"
-      add_index :post, :AuthorId
     end
   end
 end
