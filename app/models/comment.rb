597f1506cf21566b7dc6fa32bc4977c47d832788
Saied Gaffer <saied2421998@gmail.com>
2022-06-13 19:14:11 +0200

    finish the project. fix linter errors

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,9 +1,9 @@
 class Comment < ApplicationRecord
-	belongs_to :post
-	belongs_to :author, class_name: 'User', foreign_key: 'author_id'
-    after_commit :update_comments_counter_for_post, on: :create
+  belongs_to :post
+  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+  after_commit :update_comments_counter_for_post, on: :create
 
-	def update_comments_counter_for_post
-		post.update(comments_counter:post.comments.count)
-	end
+  def update_comments_counter_for_post
+    post.update(comments_counter: post.comments.count)
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,14 +1,14 @@
 class Post < ApplicationRecord
-	belongs_to :author, class_name: 'User', foreign_key: 'author_id'
-	has_many :likes, dependent: :destroy
-	has_many :comments, dependent: :destroy
-	after_commit :update_counter_for_user, on: :create
+  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+  has_many :likes, dependent: :destroy
+  has_many :comments, dependent: :destroy
+  after_commit :update_counter_for_user, on: :create
 
-	def update_counter_for_user
-		author.update(posts_counter:author.posts.count)
-	end
-	
-	def recent_comments
-		 comments.order('created_at desc').limit(5)
-	end		
+  def update_counter_for_user
+    author.update(posts_counter: author.posts.count)
+  end
+
+  def recent_comments
+    comments.order('created_at desc').limit(5)
+  end
 end
