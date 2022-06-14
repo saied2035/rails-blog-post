34dd5f17cfbaebaedbc814c4502f9201487e1c72
Saied Gaffer <saied2421998@gmail.com>
2022-06-14 22:23:32 +0200

    fix linter errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,19 +1,17 @@
 class ApplicationController < ActionController::Base
-
   def current_user
     User.find(params[:id])
-
   end
 
   def all_users
-    User.all 
+    User.all
   end
 
   def current_post
-   User.find(params[:user_id]).posts.find(params[:id])
+    User.find(params[:user_id]).posts.find(params[:id])
   end
 
   def all_posts
-   User.find(params[:user_id])
-  end  
+    User.find(params[:user_id])
+  end
 end
