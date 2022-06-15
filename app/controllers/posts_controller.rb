49e39f8d52be80e25819fc26be637bc983b06264
Saied Gaffer <saied2421998@gmail.com>
2022-06-15 18:44:48 +0200

    fix linter errors

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,21 +7,23 @@ class PostsController < ApplicationController
   def show
     @post = current_post
   end
+
   def new
     respond_to do |format|
       format.html { render :new, locals: { post: Post.new } }
     end
   end
+
   def create
     user = current_user
-    post = Post.new(params.require(:post).permit(:title,:text))
+    post = Post.new(params.require(:post).permit(:title, :text))
     post.author = user
-      if post.save
-        flash[:success] = "Post saved successfully"
-        redirect_to user_posts_url 
-      else
-        flash[:error] = "Error: Post could not be saved"
-        redirect_to new_user_post_url
-    end 
-  end 
+    if post.save
+      flash[:success] = 'Post saved successfully'
+      redirect_to user_posts_url
+    else
+      flash[:error] = 'Error: Post could not be saved'
+      redirect_to new_user_post_url
+    end
+  end
 end
