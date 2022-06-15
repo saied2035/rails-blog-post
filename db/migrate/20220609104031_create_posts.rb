1d80b56151c0a5a144ea42bca4a9dddcdcf06238
Saied Gaffer <saied2421998@gmail.com>
2022-06-15 16:48:56 +0200

    create messsages for creating post fail or success

--- db/migrate/20220609104031_create_posts.rb
+++ db/migrate/20220609104031_create_posts.rb
@@ -2,11 +2,11 @@ class CreatePosts < ActiveRecord::Migration[7.0]
   def change
     create_table :posts do |t|
       t.references :author, null:false, index: true, foreign_key: {to_table: :users}
-      t.string :title, :limit => 32
-      t.text :text
+      t.string :title, null:false, :limit => 32
+      t.text :text, null:false
       t.timestamps
-      t.bigint :comments_counter, default: 0
-      t.bigint :likes_counter, default: 0
+      t.bigint :comments_counter, null:false, default: 0
+      t.bigint :likes_counter, null:false, default: 0
     end
   end
 end
