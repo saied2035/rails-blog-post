127791b86130d3bc5d9906f94a4c7b1fc296f281
Saied Gaffer <saied2421998@gmail.com>
2022-06-16 17:29:44 +0200

    finish test for comment model

--- app/models/like.rb
+++ app/models/like.rb
@@ -2,7 +2,6 @@ class Like < ApplicationRecord
   belongs_to :post
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
   after_commit :update_likes_counter_for_user, on: :create
-
   def update_likes_counter_for_user
     post.update(likes_counter: post.likes.count)
   end
