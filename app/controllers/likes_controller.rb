364fff76f95c69aaef4d37dcc84bd946c115864b
Saied Gaffer <saied2421998@gmail.com>
2022-06-16 18:52:55 +0200

    fix linter errors

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -6,22 +6,24 @@ class LikesController < ApplicationController
       flash[:error] = 'Error: you already liked it.'
       where_to_redirect(post)
       return
-    )  
+    )
 
     like = Like.new(author: user, post:)
-    unless like.save 
+    unless like.save
       flash[:error] = 'Error: Like could not be saved'
       where_to_redirect(post)
       return
     end
-     where_to_redirect(post)
+    where_to_redirect(post)
   end
+
   private
-   def where_to_redirect(post)
-      if params[:distination] == 'index'
-        redirect_back_or_to user_posts_url
-      else
-        redirect_back_or_to user_post_url(id: post.id)
-      end
-   end 
+
+  def where_to_redirect(post)
+    if params[:distination] == 'index'
+      redirect_back_or_to user_posts_url
+    else
+      redirect_back_or_to user_post_url(id: post.id)
+    end
+  end
 end
