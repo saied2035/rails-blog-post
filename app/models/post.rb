9f87869da518df6274311ee06f0d37ec53d01bad
Saied Gaffer <saied2421998@gmail.com>
2022-06-16 14:10:57 +0200

    add validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,12 @@ class Post < ApplicationRecord
   has_many :likes, dependent: :destroy
   has_many :comments, dependent: :destroy
   after_commit :update_counter_for_user, on: :create
-
+  
+  validates :title, presence:true, length:{minimum:4, maximum:250}
+  validates :comments_counter, presence:true, 
+  numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :likes_counter, presence:true, 
+  numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   def update_counter_for_user
     author.update(posts_counter: author.posts.count)
   end
