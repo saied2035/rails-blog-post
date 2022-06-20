e9cca11b22c1d56f11f714c4dd2798c1fcfbf21e
salimabdulai <maouslayer@gmail.com>
2022-06-20 23:46:48 +0000

    Add confirm email, Add re-route to home on sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-    root 'users#index'
+    root to: 'users#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
    resources :users, only: [:index, :show] do
    resources :posts, only: [:index, :new,:create,:show] do
