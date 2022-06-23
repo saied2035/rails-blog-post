62aee4efe0edfb2179daffcebed3544097e5f939
Saied Gaffer <saied2421998@gmail.com>
2022-06-23 21:03:58 +0200

    finish post#index test

--- spec/features/post_index_spec.rb
+++ spec/features/post_index_spec.rb
@@ -1,7 +1,64 @@
 require 'rails_helper'
 
 RSpec.describe 'posts#index', type: :feature do
-  describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+ before :each do
+    user = User.new(name: 'Saied', photo: 'https://unsplash.com/photos/F_-0BxGuVvo',
+                    bio: 'developer', email: 'saied3@gmail.com', password: '123456789')
+    user.confirm
+    user.save
+    login_as(user)
   end
+
+  before :each do
+    @user1 = User.new(name: 'Saied1', photo: 'https://unsplash.com/photos/F_-0BxGuVvo',
+                      bio: 'developer', email: 'saied1@gmail.com', password: '123456')
+    @user1.confirm
+    @user1.save
+    @user2 = User.new(name: 'Saied2', photo: 'https://unsplash.com/photos/F_-0BxGuVvo',
+                      bio: 'developer', email: 'saied2@gmail.com', password: '12345678')
+    @user2.confirm
+    @user2.save
+    post1 = Post.create(author:@user1,title:'I get bored', text: 'can someone else compete this ?')
+    Post.create(author:@user1,title:'I get bored', text: 'can someone else compete this ?')
+    Post.create(author:@user1,title:'I get bored', text: 'can someone else compete this ?')
+    Post.create(author:@user1,title:'I get bored', text: 'can someone else compete this ?')
+    Post.create(author:@user1,title:'I get bored', text: 'can someone else compete this ?')
+    Comment.create(author:@user1,post:post1,text: 'comment here!')
+    Comment.create(author:@user1,post:post1,text: 'comment here!')
+    Comment.create(author:@user1,post:post1,text: 'comment here!')
+    Comment.create(author:@user1,post:post1,text: 'comment here!')
+    Comment.create(author:@user1,post:post1,text: 'comment here!')
+    Comment.create(author:@user1,post:post1,text: 'comment here!')
+    Comment.create(author:@user1,post:post1,text: 'comment here!')
+    visit user_posts_path(user_id:@user1.id)
+  end
+  it 'see user image' do
+    expect(find_all('img').count).to eq(1)
+  end
+  it 'username of the user' do
+    expect(page).to have_content @user1.name
+  end
+  it 'see number of posts counter of  the user' do
+    expect(find_all('.post-num').count).to eq(1)
+  end
+   it 'see posts titles counter of  the user' do
+    expect(find_all('.title').count).to eq(5)
+   end
+   it 'see part of the posts text' do
+    expect(find_all('.topic').count).to eq(5)
+   end
+   it 'see five recent comments of the posts' do
+    expect(find_all('.single-comment').count).to eq(5)
+   end
+   it 'see comments and likes counter of the post' do
+    expect(find_all('.comments-likes-counter')[0]).to have_content 'Comments'
+    expect(find_all('.comments-likes-counter')[0]).to have_content 'Likes'
+   end
+   it 'see  pagination button' do
+    expect(find('#show-posts')).to have_content 'pagination'
+   end
+  it 'access specific post for the user' do
+    find(:css, "a[href='/users/#{@user1.id}/posts/#{@user1.posts.first.id}']").click
+    expect(page).to have_current_path(user_post_path(user_id: @user1.id, id: @user1.posts.first.id))
+  end          
 end
--- spec/features/user_show_spec.rb
+++ spec/features/user_show_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe 'users#show', type: :feature do
+
   before :each do
     user = User.new(name: 'Saied', photo: 'https://unsplash.com/photos/F_-0BxGuVvo',
                     bio: 'developer', email: 'saied3@gmail.com', password: '123456789')
@@ -18,53 +19,41 @@ RSpec.describe 'users#show', type: :feature do
                       bio: 'developer', email: 'saied2@gmail.com', password: '12345678')
     @user2.confirm
     @user2.save
+    Post.create(author:@user1,title:'I get bored', text: 'can someone else compete this ?')
+    Post.create(author:@user1,title:'I get bored', text: 'can someone else compete this ?')
+    Post.create(author:@user1,title:'I get bored', text: 'can someone else compete this ?')
+    Post.create(author:@user1,title:'I get bored', text: 'can someone else compete this ?')
+    Post.create(author:@user1,title:'I get bored', text: 'can someone else compete this ?')
+    visit user_path(id:@user1.id)
   end
 
   it 'username of the user' do
-    visit user_path(id:@user1.id)
     expect(page).to have_content @user1.name
   end
-  it 'see number of posts counter of all users' do
-    visit user_path(id:@user1.id)
+  it 'see number of posts counter of the user' do
     expect(find_all('.post-num').count).to eq(1)
   end
 
-  it 'see number of images of all users' do
-    visit user_path(id:@user1.id)
+  it 'see user image' do
     expect(find_all('img').count).to eq(1)
   end
 
   it 'see user bio'  do
-    visit user_path(id:@user1.id)
     expect(page).to have_content @user1.bio
   end
 
   it 'see user all posts buton'  do
-    visit user_path(id:@user1.id)
     expect(find('#show-posts')).to have_content 'See all posts'
   end
 
-  it 'see user recent three posts'  do
-    Post.create(author:@user1,title:'I get bored', text: 'can someone else compete this ?')
-    Post.create(author:@user1,title:'I get bored', text: 'can someone else compete this ?')
-    Post.create(author:@user1,title:'I get bored', text: 'can someone else compete this ?')
-    Post.create(author:@user1,title:'I get bored', text: 'can someone else compete this ?')
-    Post.create(author:@user1,title:'I get bored', text: 'can someone else compete this ?')
-    visit user_path(id:@user1.id)
+  it 'see user recent three posts'  do   
     expect(find_all('.post-info').count).to eq(3)
   end
   it 'access specific post for the user' do
-    Post.create(author:@user1,title:'I get bored', text: 'can someone else compete this ?')
-    Post.create(author:@user1,title:'I get bored', text: 'can someone else compete this ?')
-    Post.create(author:@user1,title:'I get bored', text: 'can someone else compete this ?')
-    Post.create(author:@user1,title:'I get bored', text: 'can someone else compete this ?')
-    Post.create(author:@user1,title:'I get bored', text: 'can someone else compete this ?')
-    visit user_path(id:@user1.id)
     find(:css, "a[href='/users/#{@user1.id}/posts/#{@user1.recent_posts.first.id}']").click
     expect(page).to have_current_path(user_post_path(user_id: @user1.id, id: @user1.recent_posts.first.id))
   end
   it 'access all posts of the user' do
-    visit user_path(id:@user1.id)
     find(:css, "a[href='/users/#{@user1.id}/posts']").click
     expect(page).to have_current_path(user_posts_path(user_id: @user1.id))
   end  
